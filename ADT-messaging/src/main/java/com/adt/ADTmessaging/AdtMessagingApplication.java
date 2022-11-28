f253a6b55ea25934e9bd29f999f274f7bdcf113b
Andrew <andrew.dorman.taylor@gmail.com>
2022-11-28 10:40:24 -0500

    Initial

---
+++ ADT-messaging/src/main/java/com/adt/ADTmessaging/AdtMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.adt.ADTmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdtMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdtMessagingApplication.class, args);
+	}
+
+}
---
+++ ADT-messaging/src/test/java/com/adt/ADTmessaging/AdtMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adt.ADTmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdtMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
